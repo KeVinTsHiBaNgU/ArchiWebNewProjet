b6aabdbb67ab6ca8b186f49419a498facaa2cd0d
TSHIBANGU Kevin <kevintshib08@gmail.com>
2023-06-25 02:47:49 +0200

    suppression sweetAlert2

--- src/app/view-projet/view-projet.component.ts
+++ src/app/view-projet/view-projet.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { ProjetService } from '../../../services/projet.service';
 import { Router } from '@angular/router';
-
-
+import Swal from 'sweetalert2/dist/sweetalert2.js';
 
 @Component({
   selector: 'app-view-projet',
@@ -37,17 +36,32 @@ export class ViewProjetComponent {
         );
       }
 
-
       deleteProjet(projetId: string) {
-        this.projetService.deleteProjet(projetId).subscribe(() => {
-          // Suppression réussie, mettez à jour la liste des compétences
-          this.getProjets();
-        }, (error) => {
-          console.error(error);
-          // Gérer l'erreur de suppression de compétence
+        Swal.fire({
+          title: 'Confirmation',
+          text: 'Êtes-vous sûr de vouloir supprimer ce projet ?',
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: 'Oui, supprimer',
+          cancelButtonText: 'Annuler'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // L'utilisateur a confirmé la suppression, procédez à la suppression du projet
+            this.projetService.deleteProjet(projetId).subscribe(
+              () => {
+                // Suppression réussie, mettez à jour la liste des projets
+                this.getProjets();
+                Swal.fire('Supprimé', 'Le projet a été supprimé avec succès', 'success');
+              },
+              (error) => {
+                console.error(error);
+                Swal.fire('Erreur', 'Une erreur s\'est produite lors de la suppression du projet', 'error');
+              }
+            );
+          }
         });
       }
-
+    
       editProjet(projetId: string) {
         // Redirigez vers la page de modification du projet avec l'identifiant du projet en tant que paramètre
         this.router.navigate(['/projet/edit', projetId]);
